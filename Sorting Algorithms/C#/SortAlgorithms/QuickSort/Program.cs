43403955afe80e0355e09d8814eaceeae49e6017
Hyman <1604723751@qq.com>
2015-11-07 10:42:07 +0800

    Qucick Sort Implementation

---
+++ Sorting Algorithms/C#/SortAlgorithms/QuickSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+            int len = numbers.Length;
+
+            // 3, 8, 7, 5, 2, 1, 9, 6, 4 ;
+            //left = 0; right = 8; pivot = 3;
+
+            //left = 0; right = 5; pivot = 3;
+            //1,8,7,5, 2, 3, 9, 6, 4,
+
+            //left = 1, right = 5; pivot = 3;
+
+            //1,3,7, 5, 2, 8,9,6,4
+
+            //left = 1; right = 4, pivot = 3;
+
+            //1,2,7, 5, 3, 8, 9, 6, 4;
+
+            //left = 2; right = 4, pivot = 3;
+            //1,2,3, 5, 7, 8, 9, 6, 4;
+
+
+            Console.WriteLine("QuickSort By Recursive Method");
+            numbers = QuickSortHelper.QuickSort(numbers, 0, len - 1);
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write("\t{0}", numbers[i]);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Sorting Algorithms/C#/SortAlgorithms/QuickSort/QuickSortHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public class QuickSortHelper
+    {
+        public static int[] QuickSort(int[] numbers, int left, int right)
+        {
+            if(left >= right)
+            {
+                return numbers;
+            }
+            Console.WriteLine("Left:{0};Right:{1}", left, right);
+            int pivot = Partition(numbers, left, right);
+            
+            //Here we will sort by recursion.
+            //Sort the left side
+            if(pivot > left + 1 )
+            {
+                Console.WriteLine("Left:{0};Right:{1}", left, pivot - 1);
+                QuickSort(numbers, left, pivot - 1);// Note: that's the trick.
+            }
+
+            //Sort the right side
+            if(pivot + 1 < right)
+            {
+                Console.WriteLine("Left:{0};Right:{1}", pivot + 1, right);
+                QuickSort(numbers, pivot + 1, right);
+            }
+
+
+            return numbers;
+        }
+
+        public static int Partition(int[] numbers, int left, int right)
+        {
+            //to set the pivot vlaue as the left number
+            int key = numbers[left];
+
+            while(left < right)
+            {
+                while (numbers[right] >= key && left < right)
+                    right--;
+
+                int temp = numbers[right];
+                numbers[right] = numbers[left];// pivot
+                numbers[left] = temp;
+
+                while (numbers[left] <= key && left < right)
+                    left++;
+
+                temp = numbers[right];//pivot
+                numbers[right] = numbers[left];
+                numbers[left] = temp;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("\t{0}", numbers[i]);
+            }
+
+            return right;
+        }
+
+    }
+}
