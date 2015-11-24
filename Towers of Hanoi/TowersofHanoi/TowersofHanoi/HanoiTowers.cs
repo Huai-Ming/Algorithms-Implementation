1811715fa630d853cc0d685481e9c6a723f6ff01
Hyman <1604723751@qq.com>
2015-11-24 14:35:54 +0800

    Add the Tower of Hanoi algorithm implementation to the solution.

---
+++ Towers of Hanoi/TowersofHanoi/TowersofHanoi/HanoiTowers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowersofHanoi
+{
+    public class HanoiTowers
+    {
+        public void MoveDisk(int diskQuantity,string positionA, string positionB, string positionC)
+        {
+            if(diskQuantity == 1)
+            {
+                //Console.WriteLine("Move disk from position {0} to position {1}", positionA, positionC);
+                Move(diskQuantity, positionA, positionC);
+                return;
+            }
+            else
+            {
+                //Move disk from A->B 
+                MoveDisk(diskQuantity - 1, positionA, positionC, positionB);
+
+                //Move disk from A->C
+                //MoveDisk(1, positionA, positionB, positionC);
+                Move(diskQuantity, positionA, positionC);
+
+                //Move disk from B->C
+                MoveDisk(diskQuantity - 1, positionB, positionA, positionC);
+            }
+        }
+
+        private void Move(int diskIndex,string from , string to)
+        {
+            Console.WriteLine("Move {0} disk from position {1} to position {2}",diskIndex, from, to);
+        }
+    }
+}
---
+++ Towers of Hanoi/TowersofHanoi/TowersofHanoi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowersofHanoi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HanoiTowers hanoi = new HanoiTowers();
+            Console.WriteLine("Please input Disk Quantity:");
+            int DiskQuantity = Convert.ToInt32(Console.ReadLine());
+            hanoi.MoveDisk(DiskQuantity, "A", "B", "C");
+            Console.ReadKey();
+        }
+    }
+}
