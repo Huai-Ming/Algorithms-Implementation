a99a6792fe88840ebe9ae8d68ce940c50af2dd50
Hyman <1604723751@qq.com>
2015-11-30 16:00:27 +0800

    Add Shell Sort algorithm implementation to the solution

--- Sorting Algorithms/C#/SortAlgorithms/InsertionSort/Program.cs
+++ Sorting Algorithms/C#/SortAlgorithms/InsertionSort/Program.cs
@@ -10,10 +10,19 @@ namespace InsertionSort
     {
         static void Main(string[] args)
         {
+            #region Insertion Sort
             int[] array = new int[]{9,7,4,3,2,5,10,1,43,22};
             InsertionSortHelper<int>.Sort(array);
             Console.WriteLine(string.Join(",", array));
+            #endregion
+
+            #region Shell Sort
+            int[] array1 = new int[] { 9, 7, 4, 3, 2, 5, 10, 1, 43, 22 };
+            InsertionSortHelper<int>.Sort(array1);
+            Console.WriteLine(string.Join(",", array1));
+            #endregion
             Console.Read();
+
         }
     }
 }
---
+++ Sorting Algorithms/C#/SortAlgorithms/InsertionSort/ShellSortHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    public class ShellSortHelper<T> where T : IComparable
+    {
+        public static void Sort(T[] entries)
+        {
+            ShellSort(entries);
+        }
+                /**
+        * Shell sort - sort with diminishing increment (descending)
+        * @param array to be sorted
+        */
+        public static void ShellSort(T[] entries)
+        {
+            int gap = entries.Length / 2;
+            while (gap > 0)
+            {
+                for (int i = 0; i < entries.Length - gap; i++) //modified insertion sort
+                {
+                    int j = i + gap;
+                    var entry = entries[j];
+                    while (j >= gap && entries[j - 1].CompareTo(entry) > 0)
+                    {
+                        entries[j] = entries[j - gap];
+                        j -= gap;
+                    }
+                    entries[j] = entry;
+                }
+                if (gap == 2) //change the gap size
+                {
+                    gap = 1;
+                }
+                else
+                {
+                    gap = (int)(gap / 2.2);
+                }
+            }
+        }
+    }
+}
