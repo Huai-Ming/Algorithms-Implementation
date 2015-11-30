734ca7856bc95f5b667e263e02ce2874db3c9f0b
Hyman <1604723751@qq.com>
2015-11-30 14:20:43 +0800

    Add Insertion Sort algorithm implementation to the solution

---
+++ Sorting Algorithms/C#/SortAlgorithms/InsertionSort/InsertionSortHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    public class InsertionSortHelper<T> where T: IComparable
+    {
+        public static void Sort(T[] entries)
+        {
+            Sort(entries, 0, entries.Length - 1);
+        }
+
+        public static void Sort(T[] entries, Int32 first, Int32 last)
+        {
+            for (var i = first + 1; i <= last; i++)
+            {
+                //We will set the first value from entries[1];
+                var entry = entries[i];
+                var j = i;
+
+                //First we will compare entries[1] > entries[0]
+                //Traverse the entries from entries[0] to entries[j-1]
+                while (j > first && entries[j - 1].CompareTo(entry) > 0)
+                    entries[j] = entries[--j];
+
+                entries[j] = entry;
+            }
+        }
+    }
+}
---
+++ Sorting Algorithms/C#/SortAlgorithms/InsertionSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[]{9,7,4,3,2,5,10,1,43,22};
+            InsertionSortHelper<int>.Sort(array);
+            Console.WriteLine(string.Join(",", array));
+            Console.Read();
+        }
+    }
+}
